64d4b6bd8b0bfc9e220c22c145be0aea6852f82e
Henrique de Carvalho <62576280+HenriqueCarvalhoSMendes@users.noreply.github.com>
2022-02-14 23:50:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+setTimeout(function loading(){
+    document.getElementsByClassName('box')[0].style.display="none";
+    document.getElementsByClassName('content')[0].style.display="block"
+}, 5000);
