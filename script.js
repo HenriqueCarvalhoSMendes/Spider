bab8dfce79f10c4c0b531621ce673d84a43dd636
Henrique de Carvalho <62576280+HenriqueCarvalhoSMendes@users.noreply.github.com>
2022-02-20 16:29:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ window.addEventListener("load", setTimeout(function loading(){
     document.getElementsByClassName('box')[0].style.display="none"
     document.getElementsByClassName('content')[0].style.display="block"
 }, 7000))
+
+
