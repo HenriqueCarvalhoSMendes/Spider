4bfacf9c0a3fec0883777ff1fc266f26636a75be
Henrique de Carvalho <62576280+HenriqueCarvalhoSMendes@users.noreply.github.com>
2022-02-20 22:18:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 window.addEventListener("load", setTimeout(function loading(){
     document.getElementsByClassName('box')[0].style.display="none"
     document.getElementsByClassName('content')[0].style.display="block"
-}, 7000))
+}, 100))
 
 
+function openPage(x, y){
+    var indice = x
+    var target = y
+    var url = 'content'+indice+'.html'
+
+    var xml = new XMLHttpRequest()
+
+    xml.onreadystatechange = function(){
+        if (xml.readyState == 4 && xml.status == 200){
+            document.getElementById(target).innerHTML = xml.responseText
+        }
+    }
+
+    xml.open("GET", url, true)
+
+    xml.send()
+   
+}
